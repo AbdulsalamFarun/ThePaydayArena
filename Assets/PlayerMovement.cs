6f6a42010170464c55d69e3d41ec47d7a3315027
Fshsd <s443005320@st.uqu.edu.sa>
2025-06-01 20:20:30 +0300

    New Control

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,58 +1,51 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
-using ECM2;
+using UnityEngine.InputSystem;
+
 public class PlayerMovement : MonoBehaviour
 {
-    private Character _character;
+    private Vector2 moveInput;
 
-    private void Awake()
+    Rigidbody rb;
+
+    public float speed = 5f;
+
+    void Start()
     {
-        // Cache controlled character
+        rb = GetComponent<Rigidbody>();
 
-        _character = GetComponent<Character>();
     }
 
-    private void Update()
+    void Update()
     {
-        // Poll movement input
-        Vector2 inputMove = new Vector2()
+        Debug.Log(moveInput);
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector3(moveInput.x, 0, moveInput.y);
+
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
         {
-            x = Input.GetAxisRaw("Horizontal"),
-            y = Input.GetAxisRaw("Vertical")
-        };
-
-        // Compose a movement direction vector in world space
-
-        Vector3 movementDirection = Vector3.zero;
-
-        movementDirection += Vector3.right * inputMove.x;
-        movementDirection += Vector3.forward * inputMove.y;
-
-        // If character has a camera assigned,
-        // make movement direction relative to this camera view direction
-
-        if (_character.camera)
-        {
-            movementDirection
-                = movementDirection.relativeTo(_character.cameraTransform);
+            Debug.Log("Jumped");
+            // Add jump logic here, e.g., apply a force to the Rigidbody
+            rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
         }
-
-        // Set character's movement direction vector
-
-        _character.SetMovementDirection(movementDirection);
-
-        // Crouch input
-
-        if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.C))
-            _character.Crouch();
-        else if (Input.GetKeyUp(KeyCode.LeftControl) || Input.GetKeyUp(KeyCode.C))
-            _character.UnCrouch();
-
-        // Jump input
-
-        if (Input.GetButtonDown("Jump"))
-            _character.Jump();
-        else if (Input.GetButtonUp("Jump"))
-            _character.StopJumping();
     }
+
+    // void OnDrawGizmos()
+    // {
+    //     Gizmos.color = Color.red;
+
+    // }
 }
 
